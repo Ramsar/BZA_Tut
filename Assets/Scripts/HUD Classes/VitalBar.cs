562f1d72120c6e14abd46baecad35b6cbc8a63f3
Ramsar <kwatsch@outlook.com>
2012-10-18 21:27:18 +0200

    CSharpMessenger Extended added (vid 44)

---
+++ Assets/Scripts/HUD Classes/VitalBar.cs
@@ -0,0 +1,34 @@
+/// <summary>
+/// Vital bar.
+/// This class is responsible for diplaying one of the vitals of a player or a mob
+/// </summary>
+
+using UnityEngine;
+using System.Collections;
+
+public class VitalBar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	/// <summary>
+	/// This method is called when the gameobject is enabled
+	/// </summary>
+	public void OnEnable () {
+		
+	}
+	
+	/// <summary>
+	/// This method is called when the gameobject is disabled
+	/// </summary>
+	public void OnDisable () {
+		
+	}
+}
